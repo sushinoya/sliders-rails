7df10540d133c34631ecd6f95eb078fd6cb69814
Suyash Shekhar <suyashshekhar@gmail.com>
2017-06-03 18:23:16 +0800

    Add homepage, about and contact us pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
+  get 'home', to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+
+  get 'contact_us', to: 'pages#contact_us'
+
 end
