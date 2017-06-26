9b7782f0a5feea6b634531045bb7d70abac001b5
Shi Tian <shitian007@Shis-MacBook-Pro.local>
2017-06-26 22:00:13 +0800

    Add React Big Calendar

--- app/javascript/packs/sliders-components/calendar.jsx
+++ app/javascript/packs/sliders-components/calendar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+BigCalendar.momentLocalizer(moment)
+
+const myEventsList = []
+const MyCalendar = props => (
+  <div>
+    <BigCalendar
+      events={myEventsList}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+);
+
 export default class Calendar extends React.Component {
   render() {
     return (
       <div>
         Calendar
+        <MyCalendar/>
       </div>
-    );
+      );
   }
 }
--- app/javascript/packs/sliders_app.jsx
+++ app/javascript/packs/sliders_app.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import 'semantic-ui-css/semantic.min.css';
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import BigCalendar from 'react-big-calendar'
+
+// Stylesheets
+import 'semantic-ui-css/semantic.min.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import Sidebar from './sliders-components/sidebar.jsx'
 import Calendar from './sliders-components/calendar.jsx'
@@ -14,9 +18,26 @@ Sidebar.propTypes = {
   name: PropTypes.string
 }
 
+const MyCalendar = props => (
+  <div>
+    <BigCalendar
+      events={myEventsList}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+);
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Sidebar />,
+    <div className="container">
+      <div className="col-md-2">
+        <Sidebar/>
+      </div>
+      <div className="col-md-10">
+        <Calendar/>
+      </div>
+    </div>,
     document.body.appendChild(document.createElement('div')),
   )
 })
